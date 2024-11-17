6e9e5f779b44a7979bacdc76667d3ac593978d53
kkamara <kamaracomputers@gmail.com>
2024-11-17 09:49:46 +0000

    Add Home Page design

--- resources/views/v1/home.blade.php
+++ resources/views/v1/home.blade.php
@@ -2,8 +2,19 @@
 
 @section("content")
     <div class="row mt-4">
-        <div class="col-md-6 offset-md-3 text-center">
-            <h1>Home Page</h1>
+        <div class="col-md-12">
+            <div class="p-5 mb-4 bg-body-tertiary rounded-3">
+                <div class="container-fluid py-5">
+                    <h1 class="display-5 fw-bold">{{ $title }}</h1>
+                    <p class="col-md-8 fs-4">Exercitation eu Lorem aliqua eiusmod eu ut ut enim.</p>
+                    <a
+                        class="btn btn-primary btn-lg"
+                        href="/"
+                    >
+                        Refresh Page
+                </a>
+                </div>
+            </div>
         </div>
     </div>
 @stop
