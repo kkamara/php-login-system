b51cf44a076f98282695b9cd5a9d9e79ce21d6e7
kkamara <kamaracomputers@gmail.com>
2024-11-17 07:40:14 +0000

    Refactor imports

--- resources/views/v1/layouts/main.blade.php
+++ resources/views/v1/layouts/main.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{ $title }} - {{ config("app.name") }}</title>
-    @vite('resources/css/app.css')
+    @vite('resources/sass/app.scss')
 </head>
 <body>
     @include("v1/layouts/nav")
