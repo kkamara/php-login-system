bc78cfd7e2861f76a0a7882ebe7e3edba708418d
kkamara <kamaracomputers@gmail.com>
2024-11-17 10:58:58 +0000

    Add login ability

--- app/Http/Controllers/V1/Auth/LoginController.php
+++ app/Http/Controllers/V1/Auth/LoginController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\V1\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
@@ -15,9 +17,40 @@ class LoginController extends Controller
     }
 
     public function loginSubmit(Request $request) {
-        return redirect()
-            ->back()
-            ->withInput()
-            ->withErrors(["Something went wrong."]);
+        $validator = Validator::make($request->all(), [
+            "email" => "required|email",
+            "password" => "required",
+            "rememberMe" => "in:true",
+        ]);
+        $credentials = [
+            "email" => filter_var($request->input("email"), FILTER_SANITIZE_STRING),
+            "password" => filter_var($request->input("password"), FILTER_SANITIZE_STRING),
+            "rememberMe" => $request->input("rememberMe") === "true" ? "true" : null,
+        ];
+
+        if ($validator->fails()) {
+            return redirect()
+                ->back()
+                ->withInput($credentials)
+                ->withErrors($validator->errors());
+        }
+
+        $attemptCredentials = array_filter($credentials, function($key) {
+            if ($key !== "rememberMe") {
+                return true;
+            }
+            return false;
+        }, ARRAY_FILTER_USE_KEY);
+        if (!Auth::attempt(
+            $attemptCredentials,
+            $credentials["rememberMe"] === "true",
+        )) {
+            return redirect()
+                ->back()
+                ->withInput($credentials)
+                ->withErrors(["The provided email and password combination is invalid."]);
+        }
+
+        return redirect()->route("home");
     }
 }
